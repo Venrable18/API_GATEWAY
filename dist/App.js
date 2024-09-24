cb1d1b33bf454f3fbcf01db84b983ba00218ef44
venrable18 <onenvictor@gmail.com>
2024-09-24 20:27:28 +0100

    checking new functionalities

--- dist/App.js
+++ dist/App.js
@@ -1,11 +1,6 @@
 "use strict";
-// import cors from 'cors';
-// import express from 'express';
-// import http from 'http';
-// import helmet from 'helmet';
-// import 'dotenv/config';
-// import addErrorHandler from './middleware/error-handler'
-// export default class App {
-//   public express: express.Application;
-//   public httpServer: http.Server;
-// }
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+class App {
+}
+exports.default = App;
